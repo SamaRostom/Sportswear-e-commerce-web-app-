f257e26c60035f765bf80cb0eacb683a2633d8bb
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-08-26 18:35:21 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <style>
+		/* css in case of a short page */
+		/* @media (min-width: 995px) {
+		body{
+			height:100vh
+		}
+		.footer{
+			position: absolute;
+			bottom:0;
+		}
+	}
+    body{
+    background-image: url("5.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+} */
+  /* input[type="number"]{
+  width:48% !important 
+}
+input[name="Emergency Number"]{
+  margin-left: 0.5rem;
+}
+
+@media (max-width: 580px) {
+  input[type="number"]{
+  width:100% !important 
+}
+input[name="Emergency Number"]{
+  margin-left: 0rem;
+}
+}
+
+.linkClick{
+  text-decoration: none;
+}
+
+.alert{
+  display: none;
+}
+
+.form-container{
+  background-color: rgba(255, 255, 255, 0.774);
+}
+
+.showBtn{
+position: absolute;
+  right:15px;
+  top:8px;
+  color: rgb(128, 128, 128);
+  cursor: pointer;
+  z-index: 99999;
+}
+input[name="Password"]{
+  border-radius: 0px .25rem .25rem 0px !important
+} */
+	</style>
+	<script>
+	function ShowPassword() {
+        var pass = document.querySelector('input[name="Password"]');
+        var icon = document.querySelector('.showBtn i');
+        if (pass.type === "password") {
+            pass.type = "text";
+            icon.setAttribute('class','far fa-eye-slash');
+        } else {
+            pass.type = "password";
+            icon.setAttribute('class','far fa-eye');
+        }
+	}
+	</script>
+</head>
+<body>
+<?php
+include "footer.php";
+?>
+<div class='col-10 col-md-7 col-lg-6 col-xl-5 m-auto form-container px-5 py-5 my-5'>
+        <h1 class='display-6 text-center'>Log in</h1>
+		<p class='text-center'>Dive in adventures with us</p>
+<form method="POST" action="" onsubmit="validate(this,event)">
+    <div class="input-group mb-3">
+        <span class="input-group-text"><i class="fas fa-at"></i></span>
+        <input type="email" class="form-control" placeholder="Insert your email" name="Email">
+    </div>
+	<div class="input-group mb-3">
+        <span class="input-group-text"><i class="fas fa-lock"></i></span>
+        <input type="password" class="form-control" placeholder="Insert your password" name="Password">
+        <div class='position-absolute showBtn' onclick="ShowPassword()"><i class='far fa-eye'></i></div>
+    </div>
+	<div class='mt-5 text-center'>
+    <input type="submit" class='btn btn-primary px-5 mb-3' value="Log in" name="Submit">
+   <input type="reset" class='btn btn-outline-dark px-5 ms-2 mb-3'>
+   <p>Don't have an account? <a class='linkClick' href="SignUp.php">Sign up for free</a></p>
+  </div>
+</form>
+</div>
+</body>
+</html>
